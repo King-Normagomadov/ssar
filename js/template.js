f1df057b3d5865c30ae0a06679021243ec43cbef
King-Normagomadov <104327135+King-Normagomadov@users.noreply.github.com>
2022-10-19 05:01:26 -0800

    10mb

--- js/template.js
+++ js/template.js
@@ -1,8 +1,4 @@
-/* 
-	Bonativo by TEMPLATE STOCK
-	templatestock.co @templatestock
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+
 
 
  $(window).scroll(function(){      
