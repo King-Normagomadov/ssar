98f16a560279c6bba3721a2956b0c9bfddc6fdc7
asir-sar <115917327+asir-sar@users.noreply.github.com>
2023-01-21 12:30:19 +0430

    new crud
    check

--- crud/routes/sample_data.js
+++ crud/routes/sample_data.js
@@ -1,4 +1,9 @@
 var express = require('express');
+const { query } = require('../database');
+const niceInvoice = require("nice-invoice");
+const multer = require('multer');
+
+
 
 var router = express.Router();
 
@@ -45,9 +50,43 @@ router.post("/add_sample_data", function(request, response, next){
 
 	var query = `
 	INSERT INTO inventory 
-	(name, type, description, unit, quantity, category, location,invoice, consideration, status,code) 
+	(name, type, description, unit, quantity, category, location,invoice, consideration, status,date) 
 	VALUES ("${name}", "${type}", "${description}", "${unit}", "${quantity}", "${category}", "${location}", "${invoice}", "${consideration}", "${status}", "${code}")
 	`;
+
+	// ----------------------------------------------------------------------
+	var storage = multer.diskStorage({
+		destination:function(request,file,callback)
+		{
+			callback(null,'./upload');
+		},
+		filename:function(request,file,callback)
+		{
+			var temp_file_arr = file.originalname.split(".");
+
+			var temp_file_name = temp_file_arr[0];
+
+			var temp_file_extension = temp_file_arr[1];
+
+			callback(null,temp_file_name+'-'+Date.now()+'-'+temp_file_extension);
+
+		}
+
+	});
+
+	var upload = multer({storage:storage}).single('status');
+
+	upload(request,response,function(error){
+		if(error)
+		{
+			return response.end('Error Uploading File');
+		}
+		// else
+		// {
+		// 	return response.end('File Uploaded Successfully');
+		// }
+	})
+// ----------------------------------------------------------------------
 
 	database.query(query, function(error, data){
 
@@ -73,7 +112,7 @@ router.post("/add_sample_data", function(request, response, next){
 	});
 	
 	router.post("/customer_sample_data", function(request, response, next){
-		//var id = request.body.id;
+		var id = request.body.code;
 		var name = request.body.name;
 		var fathername = request.body.fathername;
 		var position = request.body.position;
@@ -92,30 +131,63 @@ router.post("/add_sample_data", function(request, response, next){
 		VALUES ("${name}", "${fathername}", "${position}", "${departement}", "${phone}", "${date}", "${item_code}", "${item_name}", "${quantity}", "${consider}", "${status}")`;
 		
 		var state_update = `UPDATE inventory SET quantity = quantity - ${quantity} where id=${item_code}`;
-			function queryresult(ans){
-				database.query(ans, function(error, data){
-			
-					if(error){throw error;}	
-					else{
-						queryresult(state_update);
-						request.flash('success', 'New Item Inserted');
-						response.redirect("/sample_data");
-						}
-			
-				});
+// ------------------------------------------------------------ bills printing
+		const invoiceDetail = {
+			shipping: {
+			  name: `${name}`,
+			  address: "Inventory Management System",
+			  city: "Kabul",
+			  state: "Kabul",
+			  country: "Afghanistan",
+			  postal_code: 1001
+			},
+			items: [
+			  {
+				item: `${item_name}`,
+				description: `${consider}`,
+				quantity: `${quantity}`,
+				price:00,
+				tax: ""
+			  }
+			],
+			 subtotal: 00,
+			 total: 00,
+			 order_number: `234-632`,
+			header:{
+				company_name: "Inventory Management System",
+				company_logo: "comp.png",
+				company_address: "Ministry of Interior"
+			},
+			footer:{
+			  text: "This is only for test purpose"
+			},
+			 currency_symbol:".", 
+			date: {
+			  billing_date: `${date}`,
+			  due_date: `${date}`,
 			}
-
-		queryresult(state_insert);
+		};
 		
-		// database.query(query, function(error, data){
+// ------------------------------------------------------------ 
+		database.query(state_insert, function(error, data){
 			
-		// 	if(error){throw error;}	
-		// 	else{
-		// 		request.flash('success', 'New Item Inserted');
-		// 		response.redirect("/sample_data");
-		// 	    }
+			if(error){throw error;}	
+			else{
+				request.flash('success', 'Customer Info Recorded');
+				//response.redirect("/sample_data");
+                niceInvoice(invoiceDetail, `${name}.pdf`); // call bill printing module
+				}
 	
-		// });
+		});
+		database.query(state_update, function(error, data){
+			
+			if(error){throw error;}	
+			else{
+				request.flash('success', 'Database Updated');
+				response.redirect("/sample_data");
+				}
+	
+		});
 		
 		
 	
@@ -163,7 +235,7 @@ router.post('/edit/:id', function(request, response, next){
 	invoice = "${invoice}", 
 	consideration = "${consideration}",  
 	status = "${status}", 
-	code = "${code}" 
+	date = "${code}" 
 	WHERE id = "${id}"
 	`;
 
