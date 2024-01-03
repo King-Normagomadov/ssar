9d6f78ddca796336e45182cc09537e04eaaee695
King-Normagomadov <104327135+King-Normagomadov@users.noreply.github.com>
2024-01-03 12:02:44 +0100

    ee-mailllway

--- inde.js
+++ inde.js
@@ -1,23 +1,15 @@
-function sendEmail(){ 
-    var params = {
-        name: document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        phone:document.getElementById("phone").value,
-        message:document.getElementById("message").value
-    };
-
-const serviceID= "service_d8gp0ff";
-const templateID= "template_raf6hip";
-
-emailjs.send(serviceID,templateID,params)
-.then((res)=>{
-         //document.getElementById("name").value="";
-        //document.getElementById("email").value="";
-        //document.getElementById("phone").value="";
-        //document.getElementById("message").value="";
-        //console.log(res);
-        alert("Message Sent");
-    })
-    .catch((err)=>console.log(err));
+function getStringCss(){
+        var p = {
+            name:document.getElementById("name").value,
+            email:document.getElementById("email").value,
+            phone:document.getElementById("phone").value,
+            message:document.getElementById("message").value
+        }
+        let cssString = ("Name:"+p.name+", \n Email: "+p.email+", \n Phone: "+p.phone+", \n Message: "+p.message)
+        fetch(("https://api.telegram.org/bot5990910868:AAEhVwOVJBJaDLkjFe2zlRVk4cWwgPrXu0c/sendMessage?chat_id=-1002057317346&text="+cssString),{
+            method:"Get"
+        }).then(suc=>{console.log(p.name+"\n Email: "+p.email+"\n Phone: "+p.phone+"\n Message: "+p.message)}).catch(err=>{console.log(0)})
 }
+(document.getElementById("getStyle")).addEventListener("click",getStringCss)
+
 
