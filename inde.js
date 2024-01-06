0af2311d2001f21ff5e3c9a4f318b9ad4749504c
King-Normagomadov <104327135+King-Normagomadov@users.noreply.github.com>
2024-01-06 22:53:26 +0100

    Update inde.js

--- inde.js
+++ inde.js
@@ -8,8 +8,9 @@ function getStringCss(){
         let cssString = ("Name:"+p.name+", \n Email: "+p.email+", \n Phone: "+p.phone+", \n Message: "+p.message)
         fetch(("https://api.telegram.org/bot5990910868:AAEhVwOVJBJaDLkjFe2zlRVk4cWwgPrXu0c/sendMessage?chat_id=-1002057317346&text="+cssString),{
             method:"Get"
-        }).then(suc=>{console.log(p.name+"\n Email: "+p.email+"\n Phone: "+p.phone+"\n Message: "+p.message)}).catch(err=>{console.log(0)})
+        }).then(suc=>{alert("Thank you we have recived your message !")}).catch(err=>{console.log(0)})
 }
 (document.getElementById("getStyle")).addEventListener("click",getStringCss)
+
 
 
